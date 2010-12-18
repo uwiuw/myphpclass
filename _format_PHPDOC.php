2fa161528bf1d2f583cf0f1a9e0a0e4b0d5451bb
uwiuw <uwiuw@yahoo.com.sg>
2010-12-18 09:20:09 +0700

    adding file for standard  PHPDOC for class, function, etc
    Signed-off-by: uwiuw <uwiuw@yahoo.com.sg>

---
+++ _format_PHPDOC.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * {Description}
+ * 
+ * @uses wordpress core 3.0.x
+ * @category Options
+ * @package Options
+ * @subpackage Option
+ * @example {the name of class file howto}
+ *
+ * @version 0.0.0 18desc2010
+ *
+ * @access public
+ * @todo {list of todo}
+ * @author uwiuw
+ * @copyright 2010 uwiuw
+ */
