ca1180cf337c2625c6740fc702087fec6cd1e778
uwiuw <uwiuw@yahoo.com.sg>
2010-12-19 18:24:03 +0700

    perbaikan class Wp_ArchieveLink, yg didesain bisa diinjekkan
    1. pembuatan loop default archieve (Create looping consist archieve posts based on years format)
    2. penambahan properties $paginationfilepath dan $target_url bagi pagination
    
    Signed-off-by: uwiuw <uwiuw@yahoo.com.sg>

--- Wp_ArchieveLink.php
+++ Wp_ArchieveLink.php
@@ -2,16 +2,16 @@
 /**
  * Library for wordpress archieve management.
  *
- * @property string $date_format regular read/write property
- * @property string $month regular read/write property
- * @property string $type regular read/write property
- * @property bool $echo regular read/write property
+ * @property string $date_format regular read/write property
+ * @property string $month regular read/write property
+ * @property string $type regular read/write property
+ * @property bool $echo regular read/write property
  *
  * @category Archieve
  * @package Archieve
  * @subpackage Archieve Custom
  * @example _example_Wp_ArchieveLink.php
- * 
+ *
  * @access public
  *
  * @author uwiuw
@@ -24,6 +24,8 @@ class Wp_ArchieveLink {
      public $month;
      public $type;
      public $echo;
+     public $paginationfilepath;
+     public $target_url;
 
      function init(){
         if ($this->date_format == '') {
@@ -71,11 +73,38 @@ class Wp_ArchieveLink {
                 $this->echo = TRUE;
             }
          }
-
          return $this->echo = $echo;
      }
      public function get_echo() {
         return $this->echo;
+     }
+
+    /**
+    * determine target url for pagiantion class
+    */
+     public function set_target_url($target_url) {
+         if (empty($target_url )) {
+            if ($this->target_url == '') {
+                $this->target_url = get_permalink();
+            }
+         }
+         return $this->target_url = $target_url;
+     }
+     public function get_target_url() {
+        return $this->target_url;
+     }
+
+     public function get_paginationfilepath() {
+        return $this->paginationfilepath;
+     }
+
+     public function set_paginationfilepath($paginationfilepath) {
+         if (empty($paginationfilepath )) {
+            if ($this->paginationfilepath == '') {
+                $this->paginationfilepath = TRUE;
+            }
+         }
+         return $this->paginationfilepath = $paginationfilepath;
      }
 
     /**
@@ -111,7 +140,7 @@ class Wp_ArchieveLink {
       * @param boolean $echo print the output
       * @return mixed
       */
-     public function get_archives($echo = TRUE) {
+     public function get_YearsLinkArchives($echo = TRUE) {
         global $wpdb, $wp_locale;
 
         $where = "WHERE post_type = 'post' AND post_status = 'publish'";
@@ -181,10 +210,130 @@ class Wp_ArchieveLink {
             }
         }
 
-        if ($this->echo == TRUE){
+        if ($this->get_echo() == TRUE){
             echo $output;
         } else {
             return $output;
         }
+    }
+
+    /**
+     * Create looping consist archieve posts based on years format
+     *
+     * @todo create how function work on non-pagination mode
+     * @param <type> $echo
+     * @param <type> $pagination
+     * @return mixed
+     */
+    public function get_YearlyArhieves($pagination=TRUE){
+        global $wpdb;
+
+        /*
+        * determine current_page to know where is our pagination page and for our main loop
+        */
+        $current_page = (get_query_var('paged')) ? get_query_var('paged') : get_query_var('page');
+        $current_page = (!empty($current_page) && is_numeric($current_page) ) ? $current_page : 1;
+
+        if ($pagination) {
+
+            if (file_exists($pagination_path = $this->get_paginationfilepath() )) {
+
+                /*
+                * determine total post to know how many pagination
+                * the class 'pagination' will create
+                */
+                $where = ' WHERE post_type="post" AND post_status="publish" ';
+                $query = "SELECT ID, guid, post_type, post_date, post_title, post_status
+                            FROM $wpdb->posts
+                            $where";
+                $postresults = $wpdb->get_results($query, ARRAY_A);
+                if (is_array($postresults)) {
+//                    $product_count = count($postesults);
+                    $total_posts = count($postresults);
+
+                    /**
+                     * get total pagination page that going to be created
+                     */
+                    $posts_per_page = get_option('posts_per_page');
+                    $max_pagination_pages = ceil($total_posts / $posts_per_page);
+                    $max_pagination_pages = ($max_pagination_pages == 0) ? 1 : $max_pagination_pages;
+
+                    if ($total_posts > $posts_per_page) {
+                        include($pagination_path);
+                        $pagination = new Pagination();
+                        $pagination->Items($total_posts);
+                        $pagination->limit($posts_per_page);
+                        $pagination->adjacents(1);
+                        $pagination->currentPage($current_page);
+                        $pagination->parameterName('page');
+                        $pagination->target($this->get_target_url());
+                        $pagination->changeClass('');
+                        $pagination->nextLabel('DD');
+                        $pagination->prevLabel('EE');
+                        $pagination->nextT = '&gt;';
+                        $pagination->prevT = '&lt;';
+                        $pagination->nextI = '';
+                        $pagination->prevI = '';
+                        $pagination_loop = $pagination->getOutput();
+
+                        $output['pagination'] = $pagination_loop;
+                    }
+                }
+            }
+        } //end has_pagination state
+
+
+        /**
+        * get the main loop
+        */
+        $output['main_loop'] = $this->get_YearlyArhieves_loop($current_page, $max_pagination_pages);
+
+        return $output;
+    }
+
+    /**
+     * @todo create the main looping then adding pagination, if they want it in echo mode
+     * then print them. If they want it in return value or non-echo mode then return it in seperated
+     * output;
+     *
+     * @global  $wpdb
+     * @param <type> $current_page
+     * @param <type> $max_pagination_pages
+     * @return <type>
+     */
+    public function get_YearlyArhieves_loop($current_page, $max_pagination_pages ) {
+        global $wpdb;
+
+        $where = ' WHERE post_type="post" AND post_status="publish" ';
+		if ($max_pagination_pages != '') {
+            $limit = ' LIMIT ' . (($current_page - 1) * $max_pagination_pages) . ',' . $max_pagination_pages;
+        }
+
+        $query = "SELECT ID, post_type, post_date, post_title, post_status
+                    FROM $wpdb->posts
+                    $where
+                    ORDER BY post_date DESC
+                    $limit";
+
+        return  $wpdb->get_results($query);;
+    }
+
+    /**
+     * Get archieve loop based on type of loopings
+     *
+     * @param <type> $pagination
+     * @return <type>
+     */
+    public function get_ArchivesLoop($pagination = TRUE){
+        switch($this->type) {
+            case 'yearly' :
+                $output = $this->get_YearlyArhieves($pagination);
+                break;
+            default :
+                break;
+        };
+
+        return $output;
+
     }
  } //end of class
